66828e8565f6cb0fec0326024b646417bc9244e7
eduardorocco <eduardo_rocco@hotmail.it>
2024-10-24 16:49:03 +0200

    ADD number display cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//COUNTDOWN
+//COUNTDOWN/////////////////////////////////////////////////////////
 
 const countDown = document.getElementById('countdown')
 
@@ -27,9 +27,46 @@ const timer = setInterval(() => {
 
 }, 1000);
 
-function randomNum() {
+
+//FUNCTION: RANDOM NUMBERS BY 1 AT 50/////////////////////////////////
+
+function randomNum50() {
 
     return Math.floor(Math.random() * 50) + 1
 }
 
-console.log(randomNum())
+//console.log(randomNum50())
+
+
+//RANDOM NUMBERS CYCLE///////////////////////////////////////////////
+
+//CONST
+
+
+
+
+
+
+//console.log(liElement)
+
+/* creare 5 <li> per la <ul> che genere 5 numeri randomici con la funzione */
+
+for (let i = 0; i < 5; i++) {
+
+    
+
+
+    const numberList = document.getElementById("numbers-list")
+
+    const liElement = document.createElement("li")
+
+    const number = document.createTextNode(randomNum50())
+
+    liElement.appendChild(number)
+
+    numberList.appendChild(liElement)
+
+    console.log(i, liElement)
+
+
+}
